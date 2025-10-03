ae0eea62e6db644d8ba392facecc3c12a1ec1d93
JoShua-MacAir <shua4347@gmail.com>
2025-10-03 11:38:33 -0600

    calculate unique stars

--- exoplanet-archive-counting.py
+++ exoplanet-archive-counting.py
@@ -5,5 +5,22 @@ DoC: Oct 3, 2025
 '''
 
 # imports
+import os
 import pandas as pd
 
+directory = "/Users/joshuabartkoske/exoplanet-transit/archive-files"
+file_name = "PS_2025.10.03_10.05.09.csv"
+file_path = os.path.join(directory, file_name)
+
+# open file
+df = pd.read_csv(file_path, header=105)
+
+# count unique number of host stars
+print(len(df["hostname"].unique()))
+
+# filter based off of rotational velocity
+fast = 35 # km/s
+fast_stars = df[df['st_vsin'] > fast]
+print(fast_stars.head())
+
+print(len(df[df['st_vsin'] > fast]["hostname"].unique()))
